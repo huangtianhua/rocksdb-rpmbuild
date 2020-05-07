8bf3a9fd22225ced1b1a5759dc9a5931a1d1d983
huangtianhua <huangtianhua223@gmail.com>
2020-05-07 10:04:48 +0000

    add rpmbuild script

---
+++ rpmbuild.sh
@@ -0,0 +1,11 @@
+#!/usr/bash
+
+sudo yum install gcc gcc-c++ make wget git rpm-build rpmdevtools gdb gtest-devel -y
+rpmdev-setuptree
+cp specs/rocksdb.spec ~/rpmbuild/SPECS/
+wget https://codeload.github.com/facebook/rocksdb/tar.gz/v6.8.1 -P ~/rpmbuild/SOURCES/
+cp sources/rocksdb-6.8.1-install_path.patch ~/rpmbuild/SOURCES/
+cd ~/rpmbuild 
+rpmbuild -ba SPECS/rocksdb.spec
+
+
